37765c77d448f3bacaf799e47d89ab829d3fbd75
Priyanshu Jindal <priyanshujindal1995@gmail.com>
2015-10-10 10:43:06 +0530

    Init Commit

---
+++ quine.py
@@ -0,0 +1,25 @@
+a = [
+"---- Python Code -----",
+"a = [",
+"]",
+"print(a[5])",
+"for i in a:",
+"    print(chr(34) + i + chr(34) +',')",
+"for i in range(10, 18):",
+"    print(a[i])",
+"---- Javascript Code -----",
+"var l = [",
+"]",
+"console.log(l[1])",
+"for(i = 0;i < l.length; ++i) {",
+"    console.log(String.fromCharCode(34) + l[i] + String.fromCharCode(34) + ',')",
+"}",
+"for(i = 2;i < 8;++i) {",
+"    console.log(l[i])",
+"}",
+]
+print(a[9])
+for i in a:
+    print(chr(34) + i + chr(34) +',')
+for i in range(10, 18):
+    print(a[i])
---
+++ readme.md
@@ -0,0 +1,6 @@
+This Program runs outputs a javascript file which outputs the original file
+
+python multi.py > multi.js
+node multi.js > out.py
+
+out.py and multi.py are freaking same
